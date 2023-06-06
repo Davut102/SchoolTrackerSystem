f719191dc1c38b7e4a1c1ee7b9abf7c9f109c2d1
aliSrmrmr <119573452+aliSrmrmr@users.noreply.github.com>
2023-06-06 23:04:48 +0300

    Revert "deletion unneceseary things"
    This reverts commit 65582978223e91bd576ba5a75fcd77715dcc1616.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/screens/Login_Screen.dart';
 import 'package:provider/provider.dart';
 
+
+import './providers/auth.dart';
+
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -9,6 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
       ],
       child: MaterialApp(
           title: 'School Tracker System',
