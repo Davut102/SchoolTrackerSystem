75f5b5da0536115170b47aa23437481efa70a6d3
aliSrmrmr <119573452+aliSrmrmr@users.noreply.github.com>
2023-06-07 04:28:46 +0300

    profile page düzenlendi
    düzenleme yapıldı

--- lib/Student/ProfilePage.dart
+++ lib/Student/ProfilePage.dart
@@ -1,34 +1,52 @@
 import 'package:flutter/material.dart';
-
-import 'MainPageStudent.dart';
-
-//void main() => runApp(ProfilePage());
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class ProfilePage extends StatefulWidget {
+  final String email;
+  final String fullName;
+
+  ProfilePage({@required this.email, this.fullName});
+
   @override
   _ProfilePageState createState() => _ProfilePageState();
-
-  String email;
-  String fullName;
-  ProfilePage({@required this.email, this.fullName});
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-
-  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
+  TextEditingController _fullNameController = TextEditingController();
 
-  String _username = '';
   String _password = '';
+  String _fullName = '';
+  String _email = '';
 
-  void _login() {
-    setState(() {
-      _username = _usernameController.text;
-      _password = _passwordController.text;
-    });
+  List<Map<String,dynamic>> userData = [];
 
-    print('Kullanıcı adı: $_username');
-    print('Şifre: $_password');
+  bool _showChangePasswordButton = false;
+  bool _showChangeFullNameButton = false;
+  bool _showChangeEmailButton = false;
+
+  Future<void> fetchData() async {
+    var url = Uri.http("localhost", "/saas/userData.php", {'q': 'http'});
+    var response = await http.post(url, body:  ({
+        "email" : widget.email
+    }));
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      List<dynamic> usersData = data as List<dynamic>;
+      userData = usersData.map((data) {
+        return {
+          'email': data['email'],
+          'fullName': data['fullName'],
+          'password': data['password'],
+        };
+      }).toList();
+      setState(() {});
+    } else {
+      print('HTTP Post Request Hatası: ${response.statusCode}');
+    }
   }
 
   void _changePassword() {
@@ -39,10 +57,30 @@ class _ProfilePageState extends State<ProfilePage> {
     print('Yeni Şifre: $_password');
   }
 
+  void _changeFullName() {
+    setState(() {
+      _fullName = _fullNameController.text;
+    });
+
+    print('Yeni İsim: $_fullName');
+  }
+
+  void _changeEmail() {
+    setState(() {
+      _email = _emailController.text;
+    });
+
+    print('Yeni Email: $_email');
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    if (userData.isEmpty) {
+      fetchData();
+    }
+
     return MaterialApp(
-      
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.redAccent,
@@ -50,17 +88,22 @@ class _ProfilePageState extends State<ProfilePage> {
             onPressed: () {
               Navigator.pop(context);
             },
-            icon: Icon(Icons.arrow_back, color: Colors.cyan,),
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.cyan,
+            ),
           ),
-
-          title: Text('Profile', style: TextStyle(fontFamily: 'Jua', fontSize: 25),),
+          title: Text(
+            'Profile',
+            style: TextStyle(fontFamily: 'Jua', fontSize: 25),
+          ),
         ),
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
               Container(
                 width: double.infinity,
-                height: 200,
+                height: 150,
                 child: CircleAvatar(
                   backgroundImage: NetworkImage(
                     'https://www2.deloitte.com/content/dam/insights/us/articles/3523_Success-by-design/images/Success-by-design_1440x660.jpg/_jcr_content/renditions/cq5dam.web.1440.660.jpeg',
@@ -73,36 +116,158 @@ class _ProfilePageState extends State<ProfilePage> {
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
-                  fontFamily: 'Jua'
+                  fontFamily: 'Jua',
                 ),
               ),
               SizedBox(height: 20),
-              Text('E-mail',style: TextStyle(fontFamily: 'Jua'),),
+              Text(
+                'Full Name',
+                style: TextStyle(fontFamily: 'Jua'),
+              ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _usernameController,
-
+                child: Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey[200],
+                  ),
+                  child: TextField(
+                    controller: _fullNameController,
+                    onTap: () {
+                      setState(() {
+                        _showChangePasswordButton = false;
+                        _showChangeFullNameButton = true;
+                        _showChangeEmailButton = false;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(10),
+                      hintText: userData.isNotEmpty ? userData[0]['fullName'] : '',
+                    ),
+                  ),
                 ),
               ),
               SizedBox(height: 16.0),
-              Text('Password',style: TextStyle(fontFamily: 'Jua'),),
+              Text(
+                'Email',
+                style: TextStyle(fontFamily: 'Jua'),
+              ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _passwordController,
-                  obscureText: true,
+                child: Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey[200],
+                  ),
+                  child: TextField(
+                    controller: _emailController,
+                    onTap: () {
+                      setState(() {
+                        _showChangePasswordButton = false;
+                        _showChangeFullNameButton = false;
+                        _showChangeEmailButton = true;
+                      });
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(10),
+                      hintText: userData.isNotEmpty ? userData[0]['email'] : '',
+                    ),
+                  ),
                 ),
               ),
               SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _changePassword,
-                child: Text('Change Password',style: TextStyle(fontFamily: 'Jua'),),
+              Text(
+                'Password',
+                style: TextStyle(fontFamily: 'Jua'),
               ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey[200],
+                  ),
+                  child: TextField(
+                    controller: _passwordController,
+                    onTap: () {
+                      setState(() {
+                        _showChangePasswordButton = true;
+                        _showChangeFullNameButton = false;
+                        _showChangeEmailButton = false;
+                      });
+                    },
+                    obscureText: !_showChangePasswordButton,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(10),
+                      hintText: _showChangePasswordButton
+                          ? userData.isNotEmpty
+                          ? userData[0]['password']
+                          : ''
+                          : '********',
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            if (_showChangePasswordButton) {
+                              _passwordController.clear();
+                            }
+                            _showChangePasswordButton = !_showChangePasswordButton;
+                          });
+                        },
+                        child: Icon(
+                          _showChangePasswordButton
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              if (_showChangePasswordButton)
+                ElevatedButton(
+                  onPressed: _changePassword,
+                  style: ElevatedButton.styleFrom(
+                    primary: Theme.of(context).primaryColor,
+                  ),
+                  child: Text(
+                    'Change Password',
+                    style: TextStyle(fontFamily: 'Jua'),
+                  ),
+                ),
+              if (_showChangeFullNameButton)
+                ElevatedButton(
+                  onPressed: _changeFullName,
+                  style: ElevatedButton.styleFrom(
+                    primary: Theme.of(context).primaryColor,
+                  ),
+                  child: Text(
+                    'Change Full Name',
+                    style: TextStyle(fontFamily: 'Jua'),
+                  ),
+                ),
+              if (_showChangeEmailButton)
+                ElevatedButton(
+                  onPressed: _changeEmail,
+                  style: ElevatedButton.styleFrom(
+                    primary: Theme.of(context).primaryColor,
+                  ),
+                  child: Text(
+                    'Change Email',
+                    style: TextStyle(fontFamily: 'Jua'),
+                  ),
+                ),
             ],
           ),
         ),
-
       ),
     );
   }
